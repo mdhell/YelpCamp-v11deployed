5319f025a9d925d7b24ac36fad51b4c2cc5caee7
mdhell <mdhellstrom@gmail.com>
2016-06-25 20:09:41 +0000

    add mongolab

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var commentRoutes        = require("./routes/comments"),
     campgroundRoutes     = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_11");
+//mongoose.connect("mongodb://localhost/yelp_camp_11");
+mongoose.connect("mongodb://Michael:dongXi88@ds023694.mlab.com:23694/hellstromyelpcamp");
+// mongodb://Michael:dongXi88@ds023694.mlab.com:23694/hellstromyelpcamp
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
